1f4770599268b4b13676c03262c495000db1ae35
mrsfreddy <rmogusu123@gmal.com>
2019-07-11 05:53:41 -0400

    add multiplication ,addition, and divition operations to my file

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = sub(number1, number2);
 alert(result);
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+var  divide = function(number1, number2) {
+  return number1 / number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
