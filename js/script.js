6a6e9f237d326263812af31921845b6a85d642be
mrsfreddy <rmogusu123@gmal.com>
2019-07-11 05:19:54 -0400

    added function to my script file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = sub(number1, number2);
+alert(result);
